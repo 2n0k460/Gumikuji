a4ee13ecf4a0d768b1f47659194d1edec57a7439
tasahara1578 <tus7414003@gmail.com>
2023-08-31 01:54:18 +0900

    なかなか問題箇所わからなくボタンを押すとアプリが閉じる

--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -2,30 +2,56 @@ package jp.wings.nikkeibp.omikuji
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.RotateAnimation
+import android.view.animation.TranslateAnimation
 import jp.wings.nikkeibp.omikuji.databinding.MainBinding
 import jp.wings.nikkeibp.omikuji.databinding.OmikujiBinding
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class OmikujiActivity : AppCompatActivity() {
     lateinit var binding: OmikujiBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = OmikujiBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 /*
         // くじ番号の取得
         val rnd = Random()
         val number = rnd.nextInt(20)
-
         // おみくじ棚の準備
         val gumikujiShelf = Array<String>(20) {"吉"}
         gumikujiShelf[0] = "大吉"
         gumikujiShelf[19] = "凶"
-        
+
         // おみくじ棚から取得
         val str = gumikujiShelf[number]
-        
+
         binding.hellowView.text = str
 */
+    }
+
+    fun onButtonClick(v:View){
+        val translate = TranslateAnimation(0f, 0f, 0f, -200f)
+        translate.repeatMode = Animation.REVERSE
+        translate.repeatCount = 5
+        translate.duration = 100
+
+        val rotate = RotateAnimation(0f, -36f,
+                        binding.imageView.width/2f, binding.imageView.height/2f)
+        rotate.duration = 200
+
+        val set = AnimationSet(true)
+        set.addAnimation(rotate)
+        set.addAnimation(translate)
+
+        binding.imageView.startAnimation(set)
+
+        binding.imageView.startAnimation(translate)
+    //    binding.imageView.setImageResource(R.drawable.result1)
     }
 }
