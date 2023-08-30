395558d5274cd312a65714aff6d8efc4b4bfd256
tasahara1578 <tus7414003@gmail.com>
2023-08-31 07:51:24 +0900

    OmikujiBoxクラスにおみくじ引くコードを移管

--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -1,21 +1,21 @@
 package jp.wings.nikkeibp.omikuji
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationSet
-import android.view.animation.RotateAnimation
-import android.view.animation.TranslateAnimation
+import androidx.appcompat.app.AppCompatActivity
 import jp.wings.nikkeibp.omikuji.databinding.OmikujiBinding
 
 class OmikujiActivity : AppCompatActivity() {
+
+    val omikujiBox = OmikujiBox()
+
     lateinit var binding: OmikujiBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = OmikujiBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        omikujiBox.omikujiView = binding.imageView
 
 /*
         // くじ番号の取得
@@ -33,7 +33,9 @@ class OmikujiActivity : AppCompatActivity() {
 */
     }
 
-    public final fun onButtonClick(v:View){
+    fun onButtonClick(v:View){
+        omikujiBox.shake()
+    /*
         val translate = TranslateAnimation(0f, 0f, 0f, -200f)
         translate.repeatMode = Animation.REVERSE
         translate.repeatCount = 5
@@ -48,8 +50,7 @@ class OmikujiActivity : AppCompatActivity() {
         set.addAnimation(translate)
 
         binding.imageView.startAnimation(set)
-
-        binding.imageView.startAnimation(translate)
+*/
     //    binding.imageView.setImageResource(R.drawable.result1)
     }
 }
--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiBox.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiBox.kt
@@ -1,11 +1,35 @@
 package jp.wings.nikkeibp.omikuji
 
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.RotateAnimation
+import android.view.animation.TranslateAnimation
+import android.widget.ImageView
 import java.util.*
 
-class GumikujiBox() {
+class OmikujiBox {
+    lateinit var omikujiView: ImageView
+    var finish = false // 箱から出たか？
     val number : Int // くじ番号
     get() {
         val rnd = Random()
         return rnd.nextInt(20)
+    }
+
+    fun shake () {
+        val translate = TranslateAnimation(0f, 0f, 0f, -200f)
+        translate.repeatMode = Animation.REVERSE
+        translate.repeatCount = 5
+        translate.duration = 100
+
+        val rotate = RotateAnimation(0f, -36f,
+            omikujiView.width/2f, omikujiView.height/2f)
+        rotate.duration = 200
+
+        val set = AnimationSet(true)
+        set.addAnimation(rotate)
+        set.addAnimation(translate)
+
+        omikujiView.startAnimation(set)
     }
 }
