5fff727925de3931ccb4ef73142143ed9d0d2d7e
tasahara1578 <tus7414003@gmail.com>
2023-08-30 23:21:42 +0900

    add shelf

--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -10,16 +10,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // 文字を表示する
-        var str = "大吉"
+        // くじ番号の取得
         val rnd = Random()
-        val number = rnd.nextInt(3)
-        if (number == 0) {
-            str = "吉"
-        }
-        else if (number == 1) {
-            str = "凶"
-        }
+        val number = rnd.nextInt(20)
+
+        // おみくじ棚の準備
+        val gumikujiShelf = Array<String>(20) {"吉"}
+        gumikujiShelf[0] = "大吉"
+        gumikujiShelf[19] = "凶"
+        
+        // おみくじ棚から取得
+        val str = gumikujiShelf[number]
+        
         binding.hellowView.text = str
     }
 }
